674ec1b004c80b3f22b6d6ef5eda7c8bd4a5af24
Zoltan Koren <korenzol.007@gmail.com>
2026-05-29 23:47:55 +0200

    feat: add fixedpoint trait for generic fixed-point number

--- resonant-core/src/lib.rs
+++ resonant-core/src/lib.rs
@@ -27,7 +27,7 @@ pub mod window;
 
 pub(crate) mod simd;
 
-pub use fixed::{Q15, Q31};
+pub use fixed::{FixedPoint, Q15, Q31};
 pub use ring_buf::RingBuf;
 #[cfg(feature = "alloc")]
 pub use ring_buf_heap::HeapRingBuf;
